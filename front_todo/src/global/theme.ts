b361fb7aa40f1e3d637006b462700757e807df49
Ricardo kenji Vivas Maeda <ricardo.maeda@veget.com.br>
2021-12-08 14:46:45 -0300

    finished api

---
+++ front_todo/src/global/theme.ts
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    blueDark: '#242C5B',
+    white: '#FAFAFA',
+    orange: '#E96B24',
+    orangeLight: '#F38952',
+    gray: '#94959D',
+  },
+  fonts: {
+    Light300: '300',
+    Regular: '400',
+    Medium: '500',
+  },
+};
