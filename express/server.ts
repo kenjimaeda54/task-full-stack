29a431da9c85aa94e8c43d3486c848afbba8c493
Ricardo kenji Vivas Maeda <ricardo.maeda@veget.com.br>
2021-12-08 10:02:29 -0300

    start: started front react js

--- express/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./src/app";
-
-app.on("appStarted", () => {
-  app.listen(3031, () => {
-    console.log("Server is listening on port 3031");
-    console.log("Clicked here to see the app: http://localhost:3031");
-  });
-});
