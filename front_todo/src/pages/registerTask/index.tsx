cae891ac0bb6d60e29d6091e6b0d9deda789ea16
Ricardo kenji Vivas Maeda <ricardo.maeda@veget.com.br>
2021-12-09 13:58:44 -0300

    feature: finished style screen registerTask

--- front_todo/src/pages/registerTask/index.tsx
+++ front_todo/src/pages/registerTask/index.tsx
@@ -3,8 +3,26 @@ import { IDataTask } from "../../utils";
 import { Header } from "../../components/header";
 import { Footer } from "../../components/footer";
 import { typeIcons } from "../../utils";
+import Calendar from "../../assets/calendar.png";
+import Clock from "../../assets/clock.png";
 import api from "../../services/api";
-import { Body, Container, ListIcons, Icon, ButtonIcon } from "./styles";
+import {
+  Body,
+  Container,
+  ListIcons,
+  Icon,
+  ButtonIcon,
+  LabelText,
+  InputText,
+  TextArea,
+  Section,
+  FooterRight,
+  ButtonCheck,
+  TitleFooterRight,
+  ButtonDestroy,
+  ImgDateTime,
+  ButtonSave,
+} from "./styles";
 
 export function RegisterTask(): JSX.Element {
   const [iconSelected, setIconSelected] = useState(50);
@@ -40,6 +58,28 @@ export function RegisterTask(): JSX.Element {
             );
           })}
         </ListIcons>
+        <LabelText>Titulo</LabelText>
+        <InputText placeholder="Titulo da tarefa" />
+        <LabelText>Descrição</LabelText>
+        <TextArea placeholder="Descricao da tarefa" />
+        <LabelText>Data</LabelText>
+        <ImgDateTime>
+          <img src={Calendar} alt="Icon Calendar" width={25} height={25} />
+        </ImgDateTime>
+        <InputText type="date" />
+        <LabelText>Hora</LabelText>
+        <ImgDateTime>
+          <img src={Clock} alt="Icon time" width={25} height={25} />
+        </ImgDateTime>
+        <InputText type="time" />
+        <Section>
+          <FooterRight>
+            <ButtonCheck type="checkbox" />
+            <TitleFooterRight>Concluido</TitleFooterRight>
+          </FooterRight>
+          <ButtonDestroy>Excluir</ButtonDestroy>
+        </Section>
+        <ButtonSave>Salvar</ButtonSave>
       </Body>
       <Footer />
     </Container>
--- front_todo/src/pages/registerTask/styles.ts
+++ front_todo/src/pages/registerTask/styles.ts
@@ -7,7 +7,6 @@ interface IIconProps {
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: 100%;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -16,12 +15,12 @@ export const Container = styled.div`
 
 export const Body = styled.div`
   display: flex;
-  margin-top: 13px;
   width: 80%;
   height: 100%;
   flex-direction: column;
   padding: 15px 20px;
-  gap: 15px;
+  gap: 23px;
+  margin-bottom: 30px;
 `;
 
 export const ListIcons = styled.div`
@@ -38,6 +37,108 @@ export const Icon = styled.img<IIconProps>`
 export const ButtonIcon = styled.button`
   border: none;
   background-color: transparent;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const ImgDateTime = styled.div`
+  position: relative;
+  left: 90%;
+  top: 50px;
+`;
+
+export const LabelText = styled.span`
+  font-weight: ${({ theme }) => theme.fonts.Medium};
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 17px;
+  line-height: 19px;
+`;
+
+export const InputText = styled.input`
+  width: 100%;
+  padding: 5px 10px;
+  font-weight: ${({ theme }) => theme.fonts.Regular};
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 17px;
+  line-height: 19px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.orange};
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`;
+
+export const TextArea = styled.textarea`
+  border: 1px solid ${({ theme }) => theme.colors.orange};
+  padding: 15px 10px;
+  font-weight: ${({ theme }) => theme.fonts.Regular};
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 17px;
+  line-height: 19px;
+  resize: none;
+  border-radius: 8px;
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.gray};
+  }
+`;
+
+export const Section = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px 0px;
+`;
+
+export const FooterRight = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const ButtonCheck = styled.input`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const TitleFooterRight = styled.span`
+  font-weight: ${({ theme }) => theme.fonts.Bold};
+  color: ${({ theme }) => theme.colors.orange};
+  font-size: 19px;
+  line-height: 21px;
+`;
+
+export const ButtonDestroy = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  font-weight: ${({ theme }) => theme.fonts.Medium};
+  color: ${({ theme }) => theme.colors.blueDark};
+  font-size: 19px;
+  line-height: 21px;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const ButtonSave = styled.button`
+  display: flex;
+  width: 100%;
+  padding: 30px 0px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 35px;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.colors.orange};
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: ${({ theme }) => theme.fonts.Regular};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 19px;
+  line-height: 23px;
   cursor: pointer;
   &:hover {
     opacity: 0.7;
